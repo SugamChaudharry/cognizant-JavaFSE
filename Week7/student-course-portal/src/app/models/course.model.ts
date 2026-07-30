a08b886fbae2287bfaad623640e0b742c753cefb
sugamchaudharry <nextgenachieverss@gmail.com>
2026-07-30 10:00:01 +0530

    removing week7

--- Week7/student-course-portal/src/app/models/course.model.ts
+++
@@ -1,9 +0,0 @@
-// Step 59: Course model used across the entire application
-export interface Course {
-  id: number;
-  name: string;
-  code: string;
-  credits: number;
-  gradeStatus: 'passed' | 'failed' | 'pending';
-  enrolled: boolean;
-}
